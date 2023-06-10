c56b77245025840af4da4312d13ed4b65ab9efb2
dong young <dong5854@gmail.com>
2023-06-10 15:21:46 +0900

    first commit

---
+++ src/main/java/com/triplenova/spacefoodtrip/SpaceFoodTripApplication.java
@@ -0,0 +1,13 @@
+package com.triplenova.spacefoodtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFoodTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceFoodTripApplication.class, args);
+    }
+
+}
