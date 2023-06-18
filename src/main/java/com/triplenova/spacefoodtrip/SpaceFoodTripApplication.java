e50fd6575d6756e7b9010635359844d5d7bf993c
dong young <dong5854@gmail.com>
2023-06-19 01:44:41 +0900

    feat: spring security 비활성화

--- src/main/java/com/triplenova/spacefoodtrip/SpaceFoodTripApplication.java
+++ src/main/java/com/triplenova/spacefoodtrip/SpaceFoodTripApplication.java
@@ -2,8 +2,9 @@ package com.triplenova.spacefoodtrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpaceFoodTripApplication {
 
     public static void main(String[] args) {
